07cc97fb0088b5eaf7c2753edcae8ef10da418d9
pjmeyer95 <pjmeyer95@gmail.com>
2023-04-03 20:48:27 -0600

    another commit

--- assets/script.js
+++ assets/script.js
@@ -24,22 +24,53 @@ async function getRandomCocktail() {
   }
 
 // search by ingredient
-async function searchCocktailsByIngredient(ingredient) {
+async function searchCocktailsByIngredients(ingredients) {
     const apiKey = "1";
-    const endpoint = `https://www.thecocktaildb.com/api/json/v1/${apiKey}/filter.php?i=${ingredient}`;
-    try {
-      const response = await fetch(endpoint);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
+    const searchResults = [];
+    for (const ingredient of ingredients) {
+      const endpoint = `https://www.thecocktaildb.com/api/json/v1/${apiKey}/filter.php?i=${ingredient}`;
+      try {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        searchResults.push(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    return searchResults;
+  }
+  searchCocktailsByIngredients(["gin", "vodka", "tequila", "rum", "whiskey", "brandy"]).then((results) => {
+    console.log(results);
+  });
+  const searchForm = document.querySelector('#search-form');
+  const resultsDiv = document.querySelector('#results');
+  
+  searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const ingredientsInput = document.querySelector('#ingredients');
+    const ingredients = ingredientsInput.value.split(',').map((ingredient) => ingredient.trim());
+    searchCocktailsByIngredients(ingredients).then((results) => {
+      displayResults(results);
+    });
+  });
+  // display search results
+  function displayResults(results) {
+    resultsDiv.innerHTML = '';
+    for (const result of results) {
+      const title = document.createElement('h2');
+      title.textContent = result.ingredients[0].toUpperCase() + result.ingredients.slice(1) + ' Cocktails';
+      resultsDiv.appendChild(title);
+      const cocktails = result.drinks;
+      const list = document.createElement('ul');
+      for (const cocktail of cocktails) {
+        const listItem = document.createElement('li');
+        const cocktailLink = document.createElement('a');
+        cocktailLink.textContent = cocktail.strDrink;
+        cocktailLink.href = `/cocktail/${cocktail.idDrink}`;
+        listItem.appendChild(cocktailLink);
+        list.appendChild(listItem);
+      }
+      resultsDiv.appendChild(list);
     }
   }
-  searchCocktailsByIngredient("gin").then((data) => {
-    console.log(data);
-  });
-  
-  searchCocktailsByIngredient("vodka").then((data) => {
-    console.log(data);
-  });
   
