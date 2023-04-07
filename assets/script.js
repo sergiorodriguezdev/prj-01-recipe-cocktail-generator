e4b328cb13fe1146187b129902e3601176df79b9
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-04-06 20:33:37 -0600

    Big major update

--- assets/script.js
+++ assets/script.js
@@ -37,24 +37,27 @@ function getUrl() {
 }  // forgot this little bugger lol
 function fetchMeal() {
   var RECIPE_URL = getUrl();
-fetch(RECIPE_URL)
-.then(function(response)
-{
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
+  fetch(RECIPE_URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
-    // define variables of what we are pulling from the meal api
+// define variables of what we are pulling from the meal api
 
-    var mealType = response.mealType; // ex: breakfast, lunch, and dinner
+// var mealType = response.mealType; // ex: breakfast, lunch, and dinner
 
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
 
 var recipes = document.getElementById("recipes-results")
 var cocktails = document.getElementById("cocktails-results")
-var recipesFormBtn = document.getElementById("recipes-res-submit")
-var cocktailsFormBtn = document.getElementById("cocktails-res-submit")
+// var recipesFormBtn = document.getElementById("recipes-res-submit")
+var cocktailsFormBtn = document.getElementById("results-submit")
 
 var myData = [
   {
@@ -80,8 +83,8 @@ var myCocktails = [
   }
 ];
 
-loadResults(recipes, "recipes", myData);
-loadResults(cocktails, "cocktails", myCocktails)
+// loadResults(recipes, "recipes", myData);
+// loadResults(cocktails, "cocktails", myCocktails)
 
 function loadResults(resultsForm, type, data) {
 
@@ -92,7 +95,7 @@ function loadResults(resultsForm, type, data) {
     //no results found
   } else {
     if (type === "recipes") {
-      recipesFormBtn.disabled = false;
+      // recipesFormBtn.disabled = false;
     } else if (type === "cocktails") {
       cocktailsFormBtn.disabled = false;
     }
@@ -120,24 +123,24 @@ function loadResults(resultsForm, type, data) {
 }
 
 
-recipesFormBtn.addEventListener("click", function (event) {
-  event.preventDefault();
+// recipesFormBtn.addEventListener("click", function (event) {
+//   event.preventDefault();
 
-  var recipesCheckboxes = document.querySelectorAll("#recipes-results input[type=checkbox]:checked");
+//   var recipesCheckboxes = document.querySelectorAll("#results input[type=checkbox]:checked");
 
-  for (let i = 0; i < recipesCheckboxes.length; i++) {
-    var nextSpanSibling = recipesCheckboxes[i].nextElementSibling;
+//   for (let i = 0; i < recipesCheckboxes.length; i++) {
+//     var nextSpanSibling = recipesCheckboxes[i].nextElementSibling;
 
-    saveFavorite(nextSpanSibling.textContent, "meal");
+//     saveFavorite(nextSpanSibling.textContent, "meal");
 
-  }
-});
+//   }
+// });
 
 cocktailsFormBtn.addEventListener("click", function (event) {
 
   event.preventDefault();
 
-  var cocktailsCheckboxes = document.querySelectorAll("#cocktails-results input[type=checkbox]:checked");
+  var cocktailsCheckboxes = document.querySelectorAll("#results input[type=checkbox]:checked");
 
   for (let i = 0; i < cocktailsCheckboxes.length; i++) {
     var nextSpanSibling = cocktailsCheckboxes[i].nextElementSibling;
@@ -248,7 +251,7 @@ favoritesListEl.addEventListener("click", function (event) {
 var openModalBtn = document.getElementById("favorites-button");
 openModalBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  console.log("here s s s s s")
+  
   // Initialize modal
   var favModal = document.getElementById("modal-favorites");
   var modalInstance = M.Modal.init(favModal);
@@ -267,9 +270,11 @@ openModalBtn.addEventListener("click", function (event) {
 async function searchCocktails(cocktailName) {
   const apiKey = "1";
   const endpoint = `https://www.thecocktaildb.com/api/json/v1/${apiKey}/search.php?s=${cocktailName}`;
+  console.log(endpoint)
   try {
     const response = await fetch(endpoint);
     const data = await response.json();
+    console.log(data)
     return data;
   } catch (error) {
     console.log(error);
@@ -281,7 +286,7 @@ async function getRandomCocktail() {
   const endpoint = `https://www.thecocktaildb.com/api/json/v1/${apiKey}/random.php`;
   try {
     const response = await fetch(endpoint);
-    const data = await response.json();
+    const data = [await response.json()];
     return data;
   } catch (error) {
     console.log(error);
@@ -302,47 +307,80 @@ async function searchCocktailsByIngredients(ingredients) {
       console.log(error);
     }
   }
+  console.log(searchResults)
   return searchResults;
 }
 searchCocktailsByIngredients(["gin", "vodka", "tequila", "rum", "whiskey", "brandy"]).then((results) => {
   console.log(results);
 });
 var searchForm = document.querySelector('#search-form');
+var randomBtn = document.querySelector("#random-button");
 var resultsDiv = document.querySelector('#results');
 
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const ingredientsInput = document.querySelector('#ingredients');
+  // Create an array of ingredients from comma separated list entered in search box
   const ingredients = ingredientsInput.value.split(',').map((ingredient) => ingredient.trim());
   searchCocktailsByIngredients(ingredients).then((results) => {
     displayResults(results);
   });
 });
+
+randomBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const ingredients = ["Random"]
+  getRandomCocktail(ingredients).then((results) => {
+    displayResults(results);
+  });
+});
+
 // display search results
 function displayResults(results) {
   resultsDiv.innerHTML = '';
-  for (const result of results) {
-    const title = document.createElement('h2');
-    title.textContent = result.ingredients[0].toUpperCase() + result.ingredients.slice(1) + ' Cocktails';
-    resultsDiv.appendChild(title);
-    const cocktails = result.drinks;
-    const list = document.createElement('ul');
-    for (const cocktail of cocktails) {
-      const listItem = document.createElement('li');
-      const cocktailLink = document.createElement('a');
-      cocktailLink.textContent = cocktail.strDrink;
-      cocktailLink.href = `/cocktail/${cocktail.idDrink}`;
-      listItem.appendChild(cocktailLink);
-      list.appendChild(listItem);
-    }
-    resultsDiv.appendChild(list);
-  }
-}
 
+  const title = document.createElement('h2');
+  const ingredientsInput = document.querySelector('#ingredients');
+  title.textContent = ingredientsInput.value + " Cocktail";
+  resultsDiv.appendChild(title);
+
+  const cocktails = results[0].drinks;
+  const list = document.createElement('ul');
+
+  (cocktails.length > 1) ? title.textContent += "s" : title.textContent += ""
+
+  for (const cocktail of cocktails) {
+    const listItem = document.createElement('li');
+
+    const cocktailLink = document.createElement('span');
+    cocktailLink.textContent = cocktail.strDrink;
+    // cocktailLink.href = `/cocktail/${cocktail .idDrink}`;
+
+    var labelEl = document.createElement("label");
+
+      var checkboxEl = document.createElement("input");
+  checkboxEl.setAttribute("type", "checkbox");
+
+  labelEl.append(checkboxEl);
+
+    labelEl.appendChild(cocktailLink);
+
+    listItem.append(labelEl);
+    
+    list.appendChild(listItem);
+  }
+
+  resultsDiv.appendChild(list);
+}
 
 
 function saveFavorite(item, type) {
   var existing = JSON.parse(localStorage.getItem(LS_FAVORITES));
+  
+  if (existing === null ) {
+    existing = [];
+  }
 
   if (!existing.length) {
     existing = [];
